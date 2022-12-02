acf15d848e9f1a4f7f38d63d38cd8100b8f1d16c
FcoJavier <javierfernandezpascual916@gmail.com>
2022-12-02 10:59:31 +0100

    dia

---
+++ dia1/helloworld.py
@@ -0,0 +1,31 @@
+python
+level 1
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print(javier)
+print(fernandez pascual)
+print(españa)
+print(type(10))
+print(type(3,4))
+print(type(9,8))
+print(type(4-4))
+print(type["javier","fernandez pascual","españa"])
+print(type("javier"))
+print(type("fernandez pascual"))
+print(type("españa"))
+/level/3
+print(5-3)
+print(type(4-5))
+print(type(3x-1))
+print"fernandezz"
+print(10>7)
+print(10==7)
+print(10<7)
+print["patata", "zanahoria", "tomate"]
+print("patata", "zanahoria", "tomate")
+
