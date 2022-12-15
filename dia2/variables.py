628435c19cf9fa1a7eeaa42bc453b7e74b72c2ae
FcoJavier <javierfernandezpascual916@gmail.com>
2022-12-15 09:16:59 +0100

    guardar

---
+++ dia2/variables.py
@@ -0,0 +1,12 @@
+"dia 2 : 30 dias de programacion en python"
+fist_name="javier"
+last_name="fernandez"
+full_name="javier fernandez"
+country="spain" 
+city="jerez"
+age=16
+year=1999
+is_married = False
+is_true = True
+is_light_on = True
+first_name , last_name , full_name , country , age , is_married = 'javier' , 'fernandez' , 'javier fernandez' , 'spain' , False
