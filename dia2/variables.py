cceec6510c08adb429cd872d8e6a29f76c97d8bf
FcoJavier <javierfernandezpascual916@gmail.com>
2022-12-16 11:07:32 +0100

    guadar

--- dia2/variables.py
+++ dia2/variables.py
@@ -10,3 +10,14 @@ is_married = False
 is_true = True
 is_light_on = True
 first_name , last_name , full_name , country , age , is_married = 'javier' , 'fernandez' , 'javier fernandez' , 'spain' , False
+print(type('javier'))
+print(type(first_name))
+print(type(12))
+print(type(4,56))
+print(type(1+3))
+print(type(False))
+print(type([4, 5, 6, 7,]))
+print(type({'javier':'fernandez','age':16, 'is_married':1999}))
+print(type((2,3)))
+print(type(zip[3,4],[5,6]))
+
