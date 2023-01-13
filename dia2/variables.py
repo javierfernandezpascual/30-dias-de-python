4a856ea1f113113cca649aaba60e40162ae5ad33
FcoJavier <javierfernandezpascual916@gmail.com>
2023-01-13 11:14:09 +0100

    dia2

--- dia2/variables.py
+++ dia2/variables.py
@@ -1,9 +1,9 @@
 "dia 2 : 30 dias de programacion en python"
-fist_name="javier"
-last_name="fernandez"
-full_name="javier fernandez"
-country="spain" 
-city="jerez"
+fist_name = "javier"
+last_name = "fernandez"
+full_name = "javier fernandez"
+country = "spain" 
+-city = "jerez"
 age=16
 year=1999
 is_married = False
@@ -11,7 +11,7 @@ is_true = True
 is_light_on = True
 first_name , last_name , full_name , country , age , is_married = 'javier' , 'fernandez' , 'javier fernandez' , 'spain' , False
 print(type('javier'))
-print(type(first_name))
+print(type(fernandez))
 print(type(12))
 print(type(4,56))
 print(type(1+3))
@@ -21,3 +21,44 @@ print(type({'javier':'fernandez','age':16, 'is_married':1999}))
 print(type((2,3)))
 print(type(zip[3,4],[5,6]))
 
+print(type(javier))
+print(type(fernandez))
+number_one = 2
+number_two = 4
+number_one + number_two = 6
+number_one - number_two = 2
+number_one · number_two = 8
+number_one / number_two = 1/2
+number_one % number_two = 0
+number_one ** number_two = 16
+number_one // number_two = 2
+
+π = 3,14
+radio=int(input(20))
+area_of_the_circle=π*radio*radio
+print(el area es:"2712")
+circum_of_the_circle=2*π*radio
+print(la circunferencia="166,9")
+radio=int(input("20"))
+area_of_the_circle=π*radio*radio
+print(el area es :"2712")
+
+first_name = input("What is your name?")
+last_name =input("What is your last name?")
+age = input("How old are you?")
+country = input("What is your country?")
+print(first_name=javier)
+print(last_name=javier fernandez)
+print(age=16)
+print(country=spain)
+
+help ("keywords")
+False                 class           from            or 
+None                   continue       global           pass
+True                   def           if               raise
+and                    del           import           return
+as                     elif            in                 try
+assert                 else            is              while
+async                  except        lambda            with
+await                  finally                         yield
+break                  for            not 
