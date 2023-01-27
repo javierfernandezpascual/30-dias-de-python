88cbc00ecf81b750046142c245c7ebfc67b30c96
FcoJavier <javierfernandezpascual916@gmail.com>
2023-01-27 11:05:23 +0100

    dia3

--- dia3/operadores.py
+++ dia3/operadores.py
@@ -10,6 +10,18 @@ Enter side b: 4
 Enter side c: 3
 The perimeter of the triangle is 12
 
+base=int(input('Dame una base cualquiera para un triangulo'))
+altura=int(input('Dame una altura cualquiera para triangulo'))
+area_triangulo= base*altura/2
+print(*el area de un triangulo cuya base", base, y cuya alturab es altura area_triangulo
+
+side_a=int(input('Dame el lado 2 "a" de un triangulo:'))
+side_b=int(input('Dame el lado 2 "b" de un triangulo:'))
+side_c=int(input('Dame el lado 2 "c" de un triangulo:'))
+print ('El perimetro de un triangulo cuyos lados miden , side_a , side_b, 'y' side_c, es ) ')
+
+
+
 
 
 
