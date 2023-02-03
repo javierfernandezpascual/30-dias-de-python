fb5509a9ae8aee0876bd7c067070723ad0726ceb
FcoJavier <javierfernandezpascual916@gmail.com>
2023-02-03 11:08:03 +0100

    dia3

--- dia3/operadores.py
+++ dia3/operadores.py
@@ -13,12 +13,26 @@ The perimeter of the triangle is 12
 base=int(input('Dame una base cualquiera para un triangulo'))
 altura=int(input('Dame una altura cualquiera para triangulo'))
 area_triangulo= base*altura/2
-print(*el area de un triangulo cuya base", base, y cuya alturab es altura area_triangulo
+print(*el area de un triangulo cuya base", base, y cuya altura b es altura area_triangulo
 
 side_a=int(input('Dame el lado 2 "a" de un triangulo:'))
 side_b=int(input('Dame el lado 2 "b" de un triangulo:'))
 side_c=int(input('Dame el lado 2 "c" de un triangulo:'))
-print ('El perimetro de un triangulo cuyos lados miden , side_a , side_b, 'y' side_c, es ) ')
+print (('El perimetro de un triangulo cuyos lados miden , side_a , side_b, 'y' side_c, es '))
+
+let length_num = parseInt(prompt("enter radius number"))
+let width_num = parseInt(prompt("enter radious widht"))
+let reactangulo_area = length_num * width_num
+console.log(rectangulo-area)
+
+let_side_a = parseInt(prompt("enter side a"))
+let_side_b = parseInt(prompt("enter side b"))
+let_side_c = parseInt(prompt("enter side c"))
+let triangle-perimeter = side a + side b + side c 
+console.log("el perimetro del rectangulo es " + triangle_perimeter)
+
+const PI = 3,14
+
 
 
 
