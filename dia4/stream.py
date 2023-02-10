349478154f266b2cf0478bda01ab9f988d8fbdce
FcoJavier <javierfernandezpascual916@gmail.com>
2023-02-10 11:15:33 +0100

    Dia 4 empezado

---
+++ dia4/stream.py
@@ -0,0 +1,16 @@
+concatunate = ['Thirty', 'Days', 'Of', Python']
+full_sentence = ' ' .join(concatunate)
+print(full_sentence)
+
+string = ['Coding', 'For', 'All']
+string_sentence = ' '.join(string)
+print(striing)
+
+company = 'Coding For All'
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.swapcase(()
+print(company.title())
