00c17586153090c89b26f75190d4722250d825e0
FcoJavier <javierfernandezpascual916@gmail.com>
2023-02-17 11:05:30 +0100

    dia4

--- dia4/stream.py
+++ dia4/stream.py
@@ -21,3 +21,11 @@ print(company lower())
 print(company capitalize())
 print(company swapcase())
 print(company title())
+
+print(company.index('Coding'))
+print(company.find('Conding'))
+print(company.replace('Coding', 'Python'))
+sentese = 'Python for Everyone'
+print(company.replace('Everyone', 'All'))
+print(company.slpit())
+
