1ceea00bff520256eecf72254909cc515913969f
FcoJavier <javierfernandezpascual916@gmail.com>
2023-02-23 09:01:54 +0100

    dia4

--- dia4/stream.py
+++ dia4/stream.py
@@ -28,4 +28,35 @@ print(company.replace('Coding', 'Python'))
 sentese = 'Python for Everyone'
 print(company.replace('Everyone', 'All'))
 print(company.slpit())
+first_letter = company[0]
+print(first_letter)
+lastLetter = company[-1]
+print(lastLetter)
+
+tenth_index = company[10]
+print(tenth_index)
+print(''.join(x[0]for w in sentence.split()))
+print(''.join(x[0]for x in company.split()))
+print(company.find(last_1,13))
+print(company.index('C'))
+print(company.index('F'))
+last_1 = '1'
+
+fist_because ='because'
+print(text.find(fist_because))
+last_because = 'because'
+print(text.find(last_because))
+print(text[21:04])
+ferst_because = 'because'
+print(text.find(ferst_because))
+because = text[21:04]
+print(because)
+print(company.startswith('Coding'))
+print(company.endswith('Coding'))
+trailing = '   Coding For All     '
+print(trailing.strip())
+
+correct_id = 'thirty_days_of_python'
+incorrect_id = '30daysofpython'
+print(correct_id.isidentifier())
 
