1a1fb878c28c5f6f0fbe142fd5a384698ebd27d2
FcoJavier <javierfernandezpascual916@gmail.com>
2023-03-03 11:06:28 +0100

    dia5

---
+++ dia5/list.py
@@ -0,0 +1,6 @@
+emptylist  = []
+
+fruits_items = ['orange', 'apple', 'watermelon', 'banana', 'mango']
+countries = ['Spain', 'Germany', 'France', 'Iran', 'Irak']
+food = ['pizza', 'chicken', 'chips', 'bread', 'sausage']
+
