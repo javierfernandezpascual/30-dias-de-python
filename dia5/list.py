40e348c8f28e4c322cfdf47770a2e285d3c37b63
FcoJavier <javierfernandezpascual916@gmail.com>
2023-03-09 09:09:31 +0100

    dia5

--- dia5/list.py
+++ dia5/list.py
@@ -3,4 +3,12 @@ emptylist  = []
 fruits_items = ['orange', 'apple', 'watermelon', 'banana', 'mango']
 countries = ['Spain', 'Germany', 'France', 'Iran', 'Irak']
 food = ['pizza', 'chicken', 'chips', 'bread', 'sausage']
+friends = ['ale', 'pablo', 'dani', 'aron', 'angel']
+numbers = ['1', '2', '3', '4', '5']
+
+print('fruits', 'Fruits')
+print('Countries', 'countries')
+print('Food', 'food')
+print('Friends', 'friends')
+print('Numbers', 'numbers')
 
